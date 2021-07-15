eb8dee564676233c48ab5adc46ece7e5f13b1fc3
unknown <yasrana@gmail.com>
2021-07-15 10:26:12 +0300

    First commit

---
+++ src/main/java/com/zetcode/PostgreSqlSpringbootApplication.java
@@ -0,0 +1,14 @@
+package com.zetcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class PostgreSqlSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgreSqlSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/zetcode/service/ICityService.java
@@ -0,0 +1,8 @@
+package com.zetcode.service;
+
+import com.zetcode.model.City;
+import java.util.List;
+
+public interface ICityService {
+	List<City> findAll();
+}
